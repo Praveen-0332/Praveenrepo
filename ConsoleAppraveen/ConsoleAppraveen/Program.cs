f8821912d5232c0c6531470ac11d1de68c6d5ced
Praveen Javvadi <Praveen.J332@outlook.com>
2019-08-07 11:31:15 +0530

    PraveenChange

---
+++ ConsoleAppraveen/ConsoleAppraveen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppraveen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Praveen";
+        }
+    }
+}
